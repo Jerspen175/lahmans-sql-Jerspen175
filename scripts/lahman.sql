6c32c7fdbe15fc18928d0d43c53551e27e185501
Jerod Spence <spencejerod@gmail.com>
2023-02-28 20:46:17 -0600

    Question 2

--- scripts/lahman.sql
+++ scripts/lahman.sql
@@ -14,7 +14,18 @@ FROM homegames
 
 The database covers games played from 1871 - 2016
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
-
+SELECT namegiven AS player_name, MIN(height) AS height, t.name AS team_name, COUNT(a.G_ALL) AS games_played
+FROM people AS p
+LEFT JOIN managershalf AS m
+USING(playerid)
+LEFT JOIN teams AS t
+USING(teamid)
+LEFT JOIN appearances AS a
+USING(playerid)
+WHERE a.gs IS NOT NULL AND t.teamid IS NOT NULL
+GROUP BY p.namegiven, height, t.name
+ORDER BY MIN(height) ASC
+LIMIT 1
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 	
