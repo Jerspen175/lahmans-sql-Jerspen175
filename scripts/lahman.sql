446ceadb190b8491e0e1244ddc09a46e4a1d97d4
Jerod Spence <spencejerod@gmail.com>
2023-03-02 21:23:04 -0600

    Working on Question 4

--- scripts/lahman.sql
+++ scripts/lahman.sql
@@ -48,9 +48,18 @@ GROUP BY namefirst, namelast
 ORDER BY SUM(salary) DESC
 David Price earned the most money($81,851,296)
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
-SELECT pos
+SELECT SUM(PO), yearid, 
+           (CASE WHEN pos = 'OF' THEN 'Outfield'
+		   WHEN pos = 'SS'  THEN 'Infield'
+		   WHEN pos = '1B' THEN 'Infield'
+		   WHEN pos = '2B' THEN 'Infield'
+		   WHEN pos = '3B' THEN 'Infield'
+		   ELSE 'Battery' END ) AS position
 FROM fielding
-HAVING 
+WHERE yearid = '2016'
+GROUP BY position, yearid
+
+
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
    
 
