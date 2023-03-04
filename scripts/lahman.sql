33eff8233936c0826117ecf6068f43896ae6b676
Jerod Spence <spencejerod@gmail.com>
2023-03-04 09:09:35 -0600

    First Push of day

--- scripts/lahman.sql
+++ scripts/lahman.sql
@@ -48,7 +48,7 @@ GROUP BY namefirst, namelast
 ORDER BY SUM(salary) DESC
 David Price earned the most money($81,851,296)
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
-SELECT SUM(PO), yearid, 
+SELECT SUM(PO) AS put_outs, yearid, 
            (CASE WHEN pos = 'OF' THEN 'Outfield'
 		   WHEN pos = 'SS'  THEN 'Infield'
 		   WHEN pos = '1B' THEN 'Infield'
